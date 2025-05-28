2406e9113808a2aa57217f094ff8c1f51c794e3e
xkolumber <lubosk158@gmail.com>
2025-05-28 17:26:17 +0200

    added matcher in middleware for /admin page

--- src/middleware.ts
+++ src/middleware.ts
@@ -28,5 +28,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/admin/:path*", "/auth/:path*"],
+  matcher: ["/admin/:path*", "/auth/:path*", "/admin"],
 };
