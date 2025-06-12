019f35a8264a8d577cd35bf394b6c651ed50d132
xkolumber <lubosk158@gmail.com>
2025-06-12 14:45:03 +0200

    detete deploy.sh and replace next env values

--- src/app/lib/awsAmplifyCognito.ts
+++ src/app/lib/awsAmplifyCognito.ts
@@ -4,8 +4,8 @@ import { Amplify, type ResourcesConfig } from "aws-amplify";
 
 export const authConfig: ResourcesConfig["Auth"] = {
   Cognito: {
-    userPoolId: String(process.env.NEXT_PUBLIC_USER_POOL_ID),
-    userPoolClientId: String(process.env.NEXT_PUBLIC_USER_POOL_CLIENT_ID),
+    userPoolId: "eu-north-1_udLWU3IVa",
+    userPoolClientId: "a8oli3n0knnmi2lbbapekpi9u",
   },
 };
 
